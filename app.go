4bd4a4e869936445f0bc0733e545f539441b006b
xingrui_zhuang <zxr@zxr-mba.local>
2024-04-02 18:25:00 +0800

    init

---
+++ app.go
@@ -0,0 +1,15 @@
+package main
+
+import "unsafe"
+
+func Atop[T any](addr *T) uintptr {
+	return uintptr(unsafe.Pointer(addr))
+}
+
+func Ptoa[T any](iptr uintptr) *T {
+	return (*T)(unsafe.Pointer(iptr))
+}
+
+func Ptoas[T []E, E any](iptr uintptr) *T {
+	return (*T)(unsafe.Pointer(iptr))
+}
